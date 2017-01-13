1121e313f626096dde5ffc510f74795e03af5ca8
Guadalupe Camacho <gcamacho079@gmail.com>
2017-01-12 19:35:52 -0800

    Completed exercises 00 and 01

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,8 @@
 #write your code here
+def hello
+  "Hello!"
+end
+
+def greet(var1)
+  "Hello, #{var1}!"
+end
--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc(var)
+  (var - 32)*(5.0/9.0)
+end
+
+def ctof(var)
+  (var * 1.8) + 32
+end
