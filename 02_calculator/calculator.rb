3af31947d1cd9ef7150a7d95cf1607ef926b38a3
Guadalupe Camacho <gcamacho079@gmail.com>
2017-01-12 19:55:26 -0800

    Progress of calculator and simon says

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,13 @@
 #write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum[var]
+  if var=nil
+    return 0
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,13 @@
 #write your code here
+def echo(var)
+  "#{var}"
+end
+
+def shout(var)
+  var.upcase!
+  "#{var}"
+end
+
+def repeat(var)
+  "#{var} #{var}"
+end
