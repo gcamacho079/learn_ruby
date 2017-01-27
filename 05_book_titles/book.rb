183c51d6dca298cfe8daa3cdb39cb31bbd78ca39
Guadalupe Camacho <gcamacho079@gmail.com>
2017-01-27 09:36:15 -0800

    Define proper_caps method to capitalize sigle-word book title

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,13 +1,24 @@
-class Book
+conjunctions = ["the", "a", "an", "and", "in", "the", "of"]
 
+class Book
   def initialize()
-    @title = "Untitled"
+
   end
 
   def title
+    @title
+  end
+
+  def title=(value)
+    @title = value
+    proper_caps
+  end
+
+  def proper_caps
+    @title =  @title.capitalize
   end
 
 end
 
 my_book = Book.new
-#my_book.title = "inferno"
+my_book.title = "inferno"
