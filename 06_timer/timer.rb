30e35fe0a9a351455b03f3068f75433579885101
Guadalupe Camacho <gcamacho079@gmail.com>
2017-02-03 12:01:30 -0800

    Define initialize and basic time_string function

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,27 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  @minutes = 0
+
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def time_string
+    if seconds < 10
+      return "00:00:0#{@seconds}"
+    elsif seconds > 10 && seconds < 60
+      return "00:00:#{@seconds}"
+    else
+      @minutes = @seconds/60
+      @seconds = @seconds - (@minutes*60)
+      return "00:#{@minutes}:#{@seconds}"
+
+    end
+  end
+
+
 end
